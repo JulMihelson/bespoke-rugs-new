ad0c6fb69c02120a572b36f1aa0edf0966fc5e50
jul.mihelson@gmail.com <125132083+JulMihelson@users.noreply.github.com>
2023-07-13 14:15:00 +0300

    test of new gallery

--- src/components/Examples/Examples.jsx
+++ src/components/Examples/Examples.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-// import css from "./Example.module.css";
+import css from "./Example.module.css";
 // import { Modal } from "./Modal";
 import PropTypes from "prop-types";
-import Zoom from "react-medium-image-zoom";
-import "react-medium-image-zoom/dist/styles.css";
+// import Zoom from "react-medium-image-zoom";
+// import "react-medium-image-zoom/dist/styles.css";
 
 export const Examples = ({ webformat }) => {
   // const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -11,22 +11,28 @@ export const Examples = ({ webformat }) => {
   //   setModalIsOpen((modalIsOpen) => !modalIsOpen);
   // };
   return (
-    <>
-      <li>
-        <Zoom>
+    <li className={css.grid}>
+      <figure className={css.effectLily}>
+        <img
+          src="https://tympanus.net/Development/HoverEffectIdeas/img/1.jpg"
+          alt="img01"
+        />
+        <figcaption>
+          <h2>
+            Nice <span>Lily</span>
+          </h2>
+          <p>Lily likes to play with crayons and pencils</p>
+        </figcaption>
+      </figure>
+      {/* <Zoom>
           <img
             // onClick={handleOnClick}
             alt="That Wanaka Tree, New Zealand by Laura Smetsers"
             src={webformat}
             width="350"
           />
-        </Zoom>
-        {/* <img onClick={handleOnClick} src={webformat} alt="килим" /> */}
-      </li>
-      {/* {modalIsOpen && (
-        <Modal handleOnClick={handleOnClick} largeImage={largeImage} />
-      )} */}
-    </>
+        </Zoom> */}
+    </li>
   );
 };
 
